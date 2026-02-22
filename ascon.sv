169f7804bc18a7707a9889096531bdaa62dbacc8
mew27 <terzolopaolo@libero.it>
2026-02-22 20:29:44 +0100

    Added round as parameter

--- ascon.sv
+++ ascon.sv
@@ -4,20 +4,24 @@ module ascon;
 reg [7:0] pc_in;
 reg [7:0] pc_out;
 
-reg [3:0] rnd;
+//reg [3:0] rnd;
 
-ascon_pc ascon_pc_inst(
+ascon_pc
+#(
+  .rnd(2)
+)
+ascon_pc_inst
+(
   .pc_in(pc_in),
-  .pc_out(pc_out),
-  .rnd(rnd)
-  );
+  .pc_out(pc_out)
+);
 
 initial begin
   pc_in = 8'hF;
-  rnd = 4'b10;
+  //rnd = 3'b10;
   $display("Hello World in System Verilog!");
   #10;
-  $display("Rnd = 0x%0h ; Pc_out = 0x%0h", rnd, pc_out);
+  $display("Rnd = 0x%0h ; Pc_out = 0x%0h", 2, pc_out);
   #10;
   $finish;
 end  
--- ascon_ps.sv
+++ ascon_ps.sv
@@ -1,5 +1,18 @@
 module ascon_ps (
-  ports
+  input logic [4:0] ps_in,
+  input logic [4:0] ps_out
 );
-  
+  always_comb begin : Sbox_comb
+    case (ps_in)
+      5'h0: ps_out = 5'h04;
+      5'h1: ps_out = 5'h0B;
+      5'h2: ps_out = 5'h1F;
+      5'h3: ps_out = 5'h14;
+      5'h4: ps_out = 5'h1A;
+      5'h5: ps_out = 5'h15;
+      5'h6: ps_out = 5'h09;
+      5'h7: ps_out = 5'h02;
+      5'h8: ps_out = 5'h1B;
+    endcase
+  end
 endmodule
